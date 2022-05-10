ebd406013a6029013eab3add915febb0ab565fbb
tak6831 <tak6831@gmail.com>
2022-05-10 20:38:48 +0900

    first commit

---
+++ src/main/java/com/bitmoi/execution/ExecutionApplication.java
@@ -0,0 +1,14 @@
+package com.bitmoi.execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class ExecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitmoi/execution/ExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitmoi.execution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
